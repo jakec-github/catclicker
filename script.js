e13870e5e954fcb9df0286c3640b45968770c76b
Jake Chorley <jakechorley@gmail.com>
2017-11-17 15:46:42 +0000

    Corrected script issues

--- script.js
+++ script.js
@@ -57,19 +57,19 @@ var view = {
       this.asideContent += cats[i].image;
       this.asideContent += '" /><h3 class="cat-preview-name">';
       this.asideContent += cats[i].name;
-      this.asideContent += '</h3></article>'
-    };
+      this.asideContent += '</h3></article>';
+    }
     this.aside.innerHTML = this.asideContent;
 
     view.renderDisplay();
     if(controller.admin){
       this.adminContent = "";
-      this.adminContent += '<div id="admin-button">Admin</div>'
+      this.adminContent += '<div id="admin-button">Admin</div>';
       this.admin.innerHTML = this.adminContent;
       this.button = document.getElementById("admin-button");
       this.button.addEventListener("click", function(){
         view.adminContent = "";
-        view.adminContent += '<label>New name</label><input id="new-name" type="text" /><button id="submit-button" type="button">Submit</button>'
+        view.adminContent += '<label>New name</label><input id="new-name" type="text" /><button id="submit-button" type="button">Submit</button>';
         view.admin.innerHTML = view.adminContent;
         view.adminSubmit();
       });
@@ -87,7 +87,7 @@ var view = {
       console.log(view.catCount);
       view.catCount.innerHTML = parseInt(view.catCount.innerHTML) + 1;
       controller.adjustModelCount(view.cat, parseInt(view.catCount.innerHTML));
-  })},
+  });},
   assignElements: function(){
     this.catPreview = document.getElementsByClassName("cat-preview");
     this.catImage = document.getElementById("cat-image");
@@ -101,16 +101,16 @@ var view = {
           view.renderDisplay();
 
         });
-    };
+    }
   },
   renderDisplay: function(){
-    console.log("Rendering display")
+    console.log("Rendering display");
     var cats = controller.getCats();
-    console.log(cats[view.cat].name)
+    console.log(cats[view.cat].name);
     view.displayContent = "";
     view.displayContent += '<h3 id="cat-name">';
     view.displayContent += cats[view.cat].name;
-    view.displayContent += '</h3><img id="cat-image" src="'
+    view.displayContent += '</h3><img id="cat-image" src="';
     view.displayContent += cats[view.cat].image;
     view.displayContent += '" /><p id="cat-count">';
     view.displayContent += cats[view.cat].count;
